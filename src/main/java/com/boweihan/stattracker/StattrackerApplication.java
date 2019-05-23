e37f33815bc8039262decf2dd5a5497a88ca7e23
bowei <bowei.han100@gmail.com>
2019-05-23 18:02:46 -0400

    initial commit (initializr)

---
+++ src/main/java/com/boweihan/stattracker/StattrackerApplication.java
@@ -0,0 +1,13 @@
+package com.boweihan.stattracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StattrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StattrackerApplication.class, args);
+	}
+
+}
