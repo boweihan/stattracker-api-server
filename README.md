36b7714283d17791553bd3564e4e9ac0131f3edd
bowei <bowei.han100@gmail.com>
2019-05-23 23:09:56 -0400

    add oauth2 resource security

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 ./mvnw flyway:migrate -Dflyway.url=jdbc:postgresql://localhost:5432/stattrackerdb -Dflyway.user=postgres -Dflyway.password=1abcdefg
 ./mvnw spring-boot:run
+
+# Generating a token
+https://oidcdebugger.com/
+url: https://dev-921890.okta.com/oauth2/default/v1/authorize
+redirectURI: https://oidcdebugger.com/debug
+scope: openid
+
+curl -v -H "Authorization: Bearer ${TOKEN}" http://localhost:8080/teachers
--- src/main/java/com/boweihan/stattracker/StattrackerApplication.java
+++ src/main/java/com/boweihan/stattracker/StattrackerApplication.java
@@ -2,6 +2,9 @@ package com.boweihan.stattracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -12,4 +15,14 @@ public class StattrackerApplication {
 		SpringApplication.run(StattrackerApplication.class, args);
 	}
 
+	@Configuration
+	static class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.authorizeRequests().anyRequest().authenticated()
+					.and()
+					.oauth2ResourceServer().jwt();
+		}
+	}
 }
